85b3f7950782b232a09027ba9adc86b34a424640
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2024-05-23 19:27:39 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,  // This will clean the output directory before each build
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,  // Enable hot reloading
+    open: true, // Automatically open the browser
+  },
+};
